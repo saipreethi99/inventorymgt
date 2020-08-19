5d57760618f2bd56397775ac3d7315c23db80575
csaipreethi <saipreethi1212@gmail.com>
2020-08-19 10:38:05 +0530

    sprint2

--- src/main/java/com/dxctraining/inventorymgt/item/entities/Item.java
+++ src/main/java/com/dxctraining/inventorymgt/item/entities/Item.java
@@ -1,73 +1,66 @@
 package com.dxctraining.inventorymgt.item.entities;
 
 import javax.persistence.Entity;
-
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import com.dxctraining.inventorymgt.supplier.entities.Supplier;
 
-//@Inheritance(strategy = InheritanceType.JOINED)
+
+
 @Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 @Table(name = "items")
 public class Item {
-	
-	@Id
-	@GeneratedValue
+	 @Id
+	 @GeneratedValue
 	private int id;
 	private String name;
 	@ManyToOne
 	private Supplier supplier;
-
-	public Item(String name, Supplier supplier) {
-		this.name = name;
-		this.supplier = supplier;
+	public Item(String name,Supplier supplier) {
+		this.name=name;
+		this.supplier=supplier;
 	}
-
-	public Item() {
-
-	}
-
+	public Item() {}
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public Supplier getSupplier() {
 		return supplier;
 	}
-
 	public void setSupplier(Supplier supplier) {
 		this.supplier = supplier;
 	}
-
-	@Override
-	public int hashCode() {
-		return id;
-	}
-
 	@Override
 	public boolean equals(Object arg) {
-		if (this == arg)
+		if(this==arg) {
 			return true;
-		if (arg == null || getClass() != arg.getClass()) {
-			return false;
 		}
-		Item that = (Item) arg;
-		return id == that.id;
+
+	   if(arg==null || !(arg instanceof Item) ) {
+			return false;
+	   }
+
+	    Item that=(Item)arg;	
+		boolean isequal=this.id==that.id;
+		return isequal;
 	}
 
+    @Override
+    public int hashCode(){
+		return id;
+		}
 }

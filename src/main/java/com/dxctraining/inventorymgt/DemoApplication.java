257de49445aa971597354bbbd79761133a74c86a
csaipreethi <saipreethi1212@gmail.com>
2020-08-20 14:38:17 +0530

    sprint6

--- src/main/java/com/dxctraining/inventorymgt/DemoApplication.java
+++ src/main/java/com/dxctraining/inventorymgt/DemoApplication.java
@@ -1,5 +1,4 @@
 package com.dxctraining.inventorymgt;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/dxctraining/inventorymgt/SupplierController.java
+++ src/main/java/com/dxctraining/inventorymgt/SupplierController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.dxctraining.inventorymgt.supplier.entities.Supplier;
 import com.dxctraining.inventorymgt.supplier.service.ISupplierService;
 
